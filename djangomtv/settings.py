9492f93cf749f318cb4f046b6cfaf0c6f9582d71
32184939 허찬용 <yong5835@naver.com>
2021-06-27 15:26:55 +0000

    Insta_clone_django

--- djangomtv/settings.py
+++ djangomtv/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'djangomtv.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -123,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Notice(models.Model):
+    title = models.CharField(max_length=100)
+    likeCount = models.IntegerField()
+    viewCount = models.IntegerField()
+    contents = models.TextField()
+
+    def __str__(self):
+        return f'{self.title} !!! {self.likeCount}'
